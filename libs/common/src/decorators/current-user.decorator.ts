c7f6a4d7d92aa254f3f258b0bce5f8013b689e1e
Pedro <alvespedro991@gmail.com>
2024-11-15 00:47:53 +0000

    fix import

--- libs/common/src/decorators/current-user.decorator.ts
+++ libs/common/src/decorators/current-user.decorator.ts
@@ -1,5 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { UserDocument } from '../../../../apps/auth/src/users/models/user.schema';
+import { UserDocument } from '@app/common/models';
 
 const getCurrentUser = (context: ExecutionContext): UserDocument => {
   return context.switchToHttp().getRequest().user;
